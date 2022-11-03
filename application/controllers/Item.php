747855ee49e8e15677e97dfd83d7eb9ee2e1dc62
Kdroid0327 <chavankiran032@gmail.com>
2022-11-03 11:12:20 +0530

    checkbox-aprecomm version 1.0

---
+++ application/controllers/Item.php
@@ -0,0 +1,62 @@
+<?php
+   
+include(APPPATH . 'libraries/REST_Controller.php');
+     
+class Item extends REST_Controller {
+    
+	  /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+       
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function index_get($id = 0)
+	{
+     
+        $this->response($data, REST_Controller::HTTP_OK);
+	}
+      
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_post()
+    {
+        $input = $this->input->post();
+     
+        $this->response(['Item created successfully.'], REST_Controller::HTTP_OK);
+    } 
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_put($id)
+    {
+        $input = $this->put();
+        $this->response(['Item updated successfully.'], REST_Controller::HTTP_OK);
+    }
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_delete($id)
+    {
+       
+        $this->response(['Item deleted successfully.'], REST_Controller::HTTP_OK);
+    }
+    	
+}
